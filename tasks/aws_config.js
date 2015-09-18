852859e9125bebd94cd75835eaa6e1f8ba6bd978
EC2 Default User <ec2-user@ip-10-0-0-36.us-west-2.compute.internal>
2015-09-18 08:39:36 +0000

    Fixed credentiasl

--- tasks/aws_config.js
+++ tasks/aws_config.js
@@ -1,9 +1,5 @@
-import { config, Credentials } from 'aws-sdk';
+import { config } from 'aws-sdk';
 
 config.region = 'us-west-2';
-const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-
-config.credentials = new Credentials(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY);
 
 export default config;
